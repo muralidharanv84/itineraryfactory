70c583e4ce84ade5c12a826e17fa084ad03a8dba
Muralidharan Venkatasubramanian <murali@muralidharan.net>
2016-06-03 15:02:24 +0530

    Initial code to fetch flight results from QPX Express API
    Added code to call and fetch results from QPX Express API. API Keys are
    hidden away from source control via local.properties and
    release.properties

---
+++ app/src/main/java/net/muralidharan/itineraryfactory/AirportAutoCompleteAdapter.java
@@ -0,0 +1,7 @@
+package net.muralidharan.itineraryfactory;
+
+/**
+ * Created by muralidharan on 6/3/16.
+ */
+public class AirportAutoCompleteAdapter {
+}
--- app/src/main/java/net/muralidharan/itineraryfactory/MainActivity.java
+++ app/src/main/java/net/muralidharan/itineraryfactory/MainActivity.java
@@ -2,12 +2,89 @@ package net.muralidharan.itineraryfactory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button findFlightsButton = (Button)findViewById(R.id.button_find_flights);
+        if (findFlightsButton != null) {
+            findFlightsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String fromAirport = ((TextView)findViewById(R.id.from_city)).getText().toString();
+                    String toAirport = ((TextView)findViewById(R.id.to_city)).getText().toString();
+                    String depDate = ((TextView)findViewById(R.id.onward_date)).getText().toString();
+                    String returnDate = ((TextView)findViewById(R.id.return_date)).getText().toString();
+                    try {
+                        findFlights(fromAirport, toAirport, depDate, returnDate);
+                    } catch (JSONException e) {
+                        Log.e(LOG_TAG, "JSONException: ", e);
+                    }
+                }
+            });
+        } else {
+            Log.w(LOG_TAG, "Could not find button to add clicklistener to!");
+        }
     }
+
+    protected void findFlights(
+            String fromAirport, String toAirport,String depDate, String returnDate) throws JSONException {
+
+        FlightsQueryRequest request = new FlightsQueryRequest(getBaseContext(), fromAirport, toAirport);
+        request.setDepDate(depDate);
+        request.setReturnDate(returnDate);
+
+        RequestQueue queue = Volley.newRequestQueue(request.getContext());
+        final String BASE_URL = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" +
+                getString(R.string.google_api_key);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                BASE_URL,
+                request.getAPIRequestJSON(20, 2),
+                new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    Log.v(LOG_TAG, "Received response: " + response.toString(4));
+                } catch (JSONException e) {
+                    Log.v(LOG_TAG, "Invalid JSON? ", e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(LOG_TAG, "Received error response: " + error.getMessage() );
+            }
+        });
+
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                120000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        queue.add(jsonObjectRequest);
+    }
+
 }
